a03c16cc1c692f4edca4904f47e8dc7fc9d87e34
Gingene <ginx2618@gmail.com>
2023-08-07 14:44:01 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,60 @@
+import "./assets/scss/all.scss";
+import "bootstrap/dist/js/bootstrap.min.js";
+
+const panelList = document.querySelector("#panelList");
+
+console.dir(panelList);
+console.log(getCurrentFileName());
+
+function panelActive() {
+  if (getCurrentFileName() === "index.html") {
+    panelList.children[2].firstElementChild.classList.add("link-success");
+    panelList.children[2].firstElementChild.classList.remove("link-dark");
+    panelList.children[2].classList.add("border-start");
+    panelList.children[2].classList.add("border-4");
+    panelList.children[2].classList.add("border-success");
+    panelList.children[4].firstElementChild.classList.add("link-dark");
+  }
+  if (getCurrentFileName() === "Admin.html") {
+    panelList.children[4].firstElementChild.classList.add("link-success");
+    panelList.children[4].firstElementChild.classList.remove("link-dark");
+    panelList.children[4].classList.add("border-start");
+    panelList.children[4].classList.add("border-4");
+    panelList.children[4].classList.add("border-success");
+    panelList.children[2].firstElementChild.classList.add("link-dark");
+  }
+}
+
+panelActive();
+
+if (getCurrentFileName() === "index.html") {
+  editModeF();
+}
+
+console.log(panelList.children[2].firstElementChild);
+
+function getCurrentFileName() {
+  let path = window.location.pathname;
+  let fileName = path.split("/").pop();
+  return fileName;
+}
+
+function editModeF() {
+  const editMode = document.querySelector("#editMode");
+  const watchMode = document.querySelector("#watchMode");
+  const watchFooter = document.querySelector("#watchFooter");
+  const editBtn = document.querySelector("#editBtn");
+  const cancelEdit = document.querySelector("#cancelEdit");
+
+  editBtn.addEventListener("click", () => {
+    editMode.classList.remove("d-none");
+    watchMode.classList.add("d-none");
+    watchFooter.classList.add("d-none");
+  });
+
+  cancelEdit.addEventListener("click", () => {
+    editMode.classList.add("d-none");
+    watchMode.classList.remove("d-none");
+    watchFooter.classList.remove("d-none");
+  });
+}
